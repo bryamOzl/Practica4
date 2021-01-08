4594ffe2604316bc4b31ee547b123ca34e12677c
bryamOzl <bryan95_19@hotmail.com>
2021-01-07 19:22:04 -0500

    Inicio Practica

---
+++ src/ups/edu/ec/clases/Logical.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ups.edu.ec.clases;
+
+/**
+ *
+ * @author Bryam
+ */
+public class Logical {
+
+    public String integrante1() {
+        return "Integrante 1";
+    }
+
+    public String integrante2() {
+        return "Integrante 2";
+    }
+}
---
+++ src/ups/edu/ec/clases/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ups.edu.ec.clases;
+
+/**
+ *
+ * @author Bryam
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Logical l =  new Logical();
+        System.out.println(l.integrante1());
+        System.out.println(l.integrante2());
+    }
+    
+}
